38eae10aed813a9a38fe16b89fd89204157209f0
clarkruan09 <ruansiqiao@gmail.com>
2023-02-19 20:39:17 -0500

    install postgres adaptor

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,12 +2,12 @@
 Sample tests
 """
 from django.test import SimpleTestCase
+
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-
 
     def test_add_numbers(self):
         """Test adding numbers together."""
